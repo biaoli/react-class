b27d66f9c102415fed99129ff6a2c03e49e6f343
biaoli <biaoli6@hotmail.com>
2023-01-06 21:00:13 -0500

    1 file modified, 1 added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
+
+import { logo } from '../utils/constants';
 
 const Navbar = () => {
   return(
---
+++ src/utils/constants.js
@@ -0,0 +1,3 @@
+
+
+export const logo = 'https://xxx/logo.png';
